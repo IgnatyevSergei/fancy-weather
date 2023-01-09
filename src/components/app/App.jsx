ff899ec74c7e7e7ca3dc46971ee08fecb24e724c
John Doe <serjmu@mail.ru>
2023-01-09 18:02:39 +0300

    add structure

---
+++ src/components/app/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ErrorBoundary from "../error-boundary";
+
+const App = () => {
+    return (
+        <ErrorBoundary>
+
+        </ErrorBoundary>
+    );
+};
+
+export default App;
